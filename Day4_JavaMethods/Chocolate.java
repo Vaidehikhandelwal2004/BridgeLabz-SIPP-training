2ddfab5fde27a3dfe82cba93cfeacfc5e7aef884
Vaidehikhandelwal2004 <khandelwaly90@gmail.com>
2025-06-18 09:50:18 +0530

    [vaidehi] Choclate

---
+++ Day4_JavaMethods/Chocolate.java
@@ -0,0 +1,18 @@
+public class Chocolate {
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[]{quotient, remainder};
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Enter number of chocolates: ");
+        int chocolates = sc.nextInt();
+        System.out.print("Enter number of children: ");
+        int children = sc.nextInt();
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Each child gets: " + result[0]);
+        System.out.println("Remaining chocolates: " + result[1]);
+    }
+}
