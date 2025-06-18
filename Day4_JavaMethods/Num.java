0125c6b1a46f6ee10eb0e7a2c40e9351dd726d83
Vaidehikhandelwal2004 <khandelwaly90@gmail.com>
2025-06-18 09:59:23 +0530

    [vaidehi] Number

---
+++ Day4_JavaMethods/Num.java
@@ -0,0 +1,43 @@
+public class Num {
+    public static void main(String[] args) {
+        int num = 145;
+        System.out.println("Perfect: " + isPerfect(num));
+        System.out.println("Abundant: " + isAbundant(num));
+        System.out.println("Deficient: " + isDeficient(num));
+        System.out.println("Strong: " + isStrong(num));
+    }
+
+    public static boolean isPerfect(int n) {
+        return sumDiv(n) == n;
+    }
+
+    public static boolean isAbundant(int n) {
+        return sumDiv(n) > n;
+    }
+
+    public static boolean isDeficient(int n) {
+        return sumDiv(n) < n;
+    }
+
+    public static boolean isStrong(int n) {
+        int temp = n, sum = 0;
+        while (temp > 0) {
+            sum += fact(temp % 10);
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+    public static int sumDiv(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++)
+            if (n % i == 0) sum += i;
+        return sum;
+    }
+
+    public static int fact(int n) {
+        int f = 1;
+        for (int i = 2; i <= n; i++) f *= i;
+        return f;
+    }
+}
