0640b9aa880825d52b0d0ab37b72634fb4bfe5ae
Technobby <khandelwaly90@gmail.com>
2025-07-01 11:50:05 +0530

    Delete Day4_javamethods directory

--- Day4_javamethods/Windy.java
+++
@@ -1,9 +0,0 @@
-public class Windy {
-    public static double chill(double t, double v) {
-        return 35.74 + 0.6215 * t - 35.75 * Math.pow(v, 0.16) + 0.4275 * t * Math.pow(v, 0.16);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(chill(10, 20));
-    }
-}
